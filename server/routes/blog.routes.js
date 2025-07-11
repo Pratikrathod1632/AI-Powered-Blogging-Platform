bfa4edcfe7c13f33c391fd93a0fd00f034fc2099
Pratikrathod1632 <rathodpratik053@gmail.com>
2025-07-11 18:39:35 +0530

    create api for list,details, delete and update

--- server/routes/blog.routes.js
+++ server/routes/blog.routes.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { addBlog } from '../controllers/blog.controllers.js';
+import { addBlog, getAllBlogs, getBlogById, togglePublish } from '../controllers/blog.controllers.js';
 import upload from '../middlewares/multer.js';
 import auth from '../middlewares/auth.js';
 
 const blogRouter = express.Router();
 
 blogRouter.post("/add",upload.single('image'), auth, addBlog)
+blogRouter.get('/all',getAllBlogs) //display all blogs
+blogRouter.get('/:blogId',getBlogById)
+blogRouter.post('/delete',auth,getBlogById) //auth is used because only admin can delete this blog
+
+blogRouter.post('/toggle-publish',auth,togglePublish) //switch to publish and unpublish
+
 
 export default blogRouter;
